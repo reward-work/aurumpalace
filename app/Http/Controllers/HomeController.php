803a098fb24ca43b0e3762b1b417eca0fbf52dc9
lebuhorskiy <lebuhorskiy65@gmail.com>
2019-09-28 21:44:21 +0300

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tag;
+use App\Post;
+use App\Category;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+//    	$posts = Post::paginate(2);
+
+    	return view('aurum.index');
+    }
+
+    public function show($slug)
+    {
+    	$post = Post::where('slug', $slug)->firstOrFail();
+
+    	return view('pages.show', compact('post'));
+    }
+
+    public function tag($slug)
+    {
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+
+        $posts = $tag->posts()->paginate(2);
+
+        return view('pages.list', ['posts'  =>  $posts]);
+    }
+
+    public function category($slug)
+    {
+        $category = Category::where('slug', $slug)->firstOrFail();
+
+        $posts = $category->posts()->paginate(2);
+
+        return view('pages.list', ['posts'  =>  $posts]);   
+    }
+}
